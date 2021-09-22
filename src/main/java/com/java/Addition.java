70227ddd7c357933efec5c58d38e0acfbf8cdeb5
unknown <altamashbagwan1@gmail.com>
2021-09-22 14:36:13 +0530

    Added: Addition

---
+++ src/main/java/com/java/Addition.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class Addition {
+	public static void main(String[] args) {
+		int num1 = 50;
+		int num2 = 29;
+		int sum;
+		sum = num1 + num2;
+		System.out.println("Sum is equal to:" +sum);
+	}
+
+}
