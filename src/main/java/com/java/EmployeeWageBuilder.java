84bc4506f4c834eea411195d509887341edb2c3d
unknown <altamashbagwan1@gmail.com>
2021-09-24 19:59:54 +0530

    Refactored: content Welcome Massage

--- src/main/java/com/java/EmployeeWageBuilder.java
+++ src/main/java/com/java/EmployeeWageBuilder.java
@@ -3,18 +3,5 @@ package com.java;
 public class EmployeeWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome Employee Wage");
-	
-	
-		int num = (int) Math.random() * 2;
-		System.out.println(num);
-		if(num == 1)
-		{
-			System.out.println("emolyee is present");
-		
-		}
-		else
-		{
-			System.out.println("employee is not present");
-		}
 	}
 }
